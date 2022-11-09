4590115ceba2223215adf32053bc3e48c7d8be1c
Muhammad Hidayat <muh.hidayaat24@gmail.com>
2022-11-10 03:32:32 +0800

    set: auth payload & cors

--- app/api/v1/auth/controller.js
+++ app/api/v1/auth/controller.js
@@ -1,13 +1,13 @@
-const { signin } = require("../../../services/mongoose/auth");
+const { signin } = require('../../../services/mongoose/auth');
 
-const { StatusCodes } = require("http-status-codes");
+const { StatusCodes } = require('http-status-codes');
 
 const signinCms = async (req, res, next) => {
   try {
     const result = await signin(req);
 
     res.status(StatusCodes.CREATED).json({
-      data: { token: result },
+      data: { token: result.token, role: result.role },
     });
   } catch (err) {
     next(err);
